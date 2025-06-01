26d87220f1b428524af4794665edfa4181f5117d
hyeonguk <shyunguk126@naver.com>
2025-06-02 01:38:55 +0900

    다형성-ì다운캐스팅

---
+++ problem-solving1/src/baekjoon/Quiz4779.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Quiz4779 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = "";
+
+        while ((input = br.readLine()) != null && !input.isEmpty()) {
+
+
+        }
+
+
+
+
+
+
+    }
+}
