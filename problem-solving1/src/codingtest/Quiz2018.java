b70392f82675adee36d36a75dd8e11fc688140c2
hyeonguk <shyunguk126@naver.com>
2025-07-07 02:17:21 +0900

    투 포ì인í터ë¬ 문제풀기 완료

---
+++ problem-solving1/src/codingtest/Quiz2018.java
@@ -0,0 +1,37 @@
+package codingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Quiz2018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int start =0, end =0;
+        int sum = 0, cnt =0;
+
+        while (start <= N) {
+            while (++end <= N) {
+                sum += end;
+                if (sum >= N) {
+                    if (sum == N) cnt++;
+                    break;
+                }
+            }
+
+            while (++start <= N) {
+                sum -= start;
+                if (sum <= N) {
+                    if (sum == N) cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
